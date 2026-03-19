bffc36bec5a190c43e26a652f370228ed6d725dc
Laffy <bidwattam04@gmail.com>
2026-03-19 15:17:56 +0530

    Added EmployeeRestController file

---
+++ src/main/java/com/capgemini/training/controller/EmployeeRestController.java
@@ -0,0 +1,25 @@
+package com.capgemini.training.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capgemini.training.entity.Employee;
+import com.capgemini.training.services.EmployeeServices;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    @Autowired
+    private EmployeeServices employeeServices;
+
+    // Get all employees
+    @GetMapping("/allemp")
+    public List<Employee> getAllEmployee() {
+        return employeeServices.getAllEmployees();
+    }
+}
